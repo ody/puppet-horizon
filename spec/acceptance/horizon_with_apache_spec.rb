47923f4aa15c1f4393a2205c7a22c2c24ce50a79
Cody Herriges <cody@herriges.org>
2016-01-15 15:32:17 -0800

    Making root_url configurable
      This commit will make universal modifications to the way the "root_url"
      parameter is set so that a person can modify it.
    
      This needs to occur to allow people the ability to serve horizon from the
      root of a vhost.
    
    Change-Id: If43ea694b2601c55a4b07c1cd5fc411abbaaaa2d

--- spec/acceptance/horizon_with_apache_spec.rb
+++ spec/acceptance/horizon_with_apache_spec.rb
@@ -15,6 +15,7 @@ describe 'horizon class' do
         # https://bugs.launchpad.net/ubuntu/+source/horizon/+bug/1424042
         compress_offline => false,
         allowed_hosts    => 'localhost',
+        server_aliases   => [$::fqdn, 'localhost'],
       }
       EOS
 
@@ -25,12 +26,52 @@ describe 'horizon class' do
 
     # basic test for now, to make sure Apache serve /horizon dashboard
     if os[:family] == 'Debian'
-      describe command('curl --connect-timeout 5 -sL -w "%{http_code} %{url_effective}\n" http://localhost/horizon/ -o /dev/null') do
+      describe command('curl --connect-timeout 5 -sL -w "%{http_code} %{url_effective}\n" http://localhost/horizon/auth/login -o /dev/null') do
         it { is_expected.to return_exit_status 0 }
+        it { is_expected.to return_stdout(/^200/) }
       end
     elsif os[:family] == 'RedHat'
-      describe command('curl --connect-timeout 5 -sL -w "%{http_code} %{url_effective}\n" http://localhost/dashboard/ -o /dev/null') do
+      describe command('curl --connect-timeout 5 -sL -w "%{http_code} %{url_effective}\n" http://localhost/dashboard/auth/login -o /dev/null') do
         it { is_expected.to return_exit_status 0 }
+        it { is_expected.to return_stdout(/^200/) }
+      end
+    end
+
+  end
+
+  context 'parameters with modified root' do
+
+    it 'should work with no errors' do
+      pp= <<-EOS
+      include ::openstack_integration
+      include ::openstack_integration::repos
+
+      class { '::horizon':
+        secret_key       => 'big_secret',
+        # need to disable offline compression due to
+        # https://bugs.launchpad.net/ubuntu/+source/horizon/+bug/1424042
+        compress_offline => false,
+        allowed_hosts    => 'localhost',
+        server_aliases   => [$::fqdn, 'localhost'],
+        root_url         => '',
+      }
+      EOS
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    # basic test for now, to make sure Apache serve /horizon dashboard
+    if os[:family] == 'Debian'
+      describe command('curl --connect-timeout 5 -sL -w "%{http_code} %{url_effective}\n" http://localhost/auth/login -o /dev/null') do
+        it { is_expected.to return_exit_status 0 }
+        it { is_expected.to return_stdout(/^200/) }
+      end
+    elsif os[:family] == 'RedHat'
+      describe command('curl --connect-timeout 5 -sL -w "%{http_code} %{url_effective}\n" http://localhost/auth/login -o /dev/null') do
+        it { is_expected.to return_exit_status 0 }
+        it { is_expected.to return_stdout(/^200/) }
       end
     end
 
